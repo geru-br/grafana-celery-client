52b6af81f061a766a582be8548897560616cf9e8
Fernando Fardo <ffardo@geru.com.br>
2017-10-23 16:49:17 -0200

    Included process name argument in decorator

--- grafana_celery_client/duration.py
+++ grafana_celery_client/duration.py
@@ -4,20 +4,31 @@ from datetime import datetime
 from tasks import send_metric
 
 
-def timeit(func, environment, metric=None, tags=None, server=None, port=None):
+def timeit(func, environment, process_name=None, metric=None, tags=None, server=None, port=None):
     '''
 
     :param func: Decorated function
     :param environment: current environment (dev, stage, production)
-    :param metric: Metric name. If none is passed, the function name will be used
+    :param process_name: Name of the process that is being measured. If none is passed, the function name will be used
+    :param metric: Metric name. If none is passed, duration will be used
     :param tags: list of tags in the form [{'key1': value1},...,{'keyN': valueN}]
     :param server: server domain name
     :param port:metric server port
     :return: This function will return whatever the decorated function returns
     '''
 
+    if not process_name:
+        process_name = func.__name__
+
+    process_tag = {'process_name': process_name}
+
+    if not tags:
+        tags = [process_tag]
+    else:
+        tags.append(process_tag)
+
     if not metric:
-        metric = func.__name__
+        metric = 'duration'
 
     def wrapper():
         start = timer()
