4b2462042296e52663200572bbc118fb629467cc
Fernando Fardo <ffardo@geru.com.br>
2017-10-23 17:38:56 -0200

    Fixed decorator arguments

--- grafana_celery_client/duration.py
+++ grafana_celery_client/duration.py
@@ -2,14 +2,13 @@
 from timeit import default_timer as timer
 from datetime import datetime
 from tasks import send_metric
+import functools
 
 
-def timeit(func, environment, process_name=None, metric=None, tags=None, server=None, port=None):
+def timeit(environment, process_name, metric=None, tags=None, server=None, port=None):
     '''
-
-    :param func: Decorated function
     :param environment: current environment (dev, stage, production)
-    :param process_name: Name of the process that is being measured. If none is passed, the function name will be used
+    :param process_name: Name of the process that is being measured.
     :param metric: Metric name. If none is passed, duration will be used
     :param tags: list of tags in the form [{'key1': value1},...,{'keyN': valueN}]
     :param server: server domain name
@@ -17,25 +16,33 @@ def timeit(func, environment, process_name=None, metric=None, tags=None, server=
     :return: This function will return whatever the decorated function returns
     '''
 
-    if not process_name:
-        process_name = func.__name__
+    def _timeit(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
 
-    process_tag = {'process_name': process_name}
+            _process_name = process_name
+            _tags = tags
+            _metric = metric
 
-    if not tags:
-        tags = [process_tag]
-    else:
-        tags.append(process_tag)
+            if not _process_name:
+                _process_name = func.__name__
 
-    if not metric:
-        metric = 'duration'
+            _process_tag = {'process_name': _process_name}
 
-    def wrapper():
-        start = timer()
-        ret = func()
-        duration = timer() - start
-        localtime = datetime.utcnow()
-        send_metric.delay(environment, metric, duration, tags, localtime, server, port)
-        return ret
+            if not _tags:
+                _tags = [_process_tag]
+            else:
+                _tags.append(_process_tag)
 
-    return wrapper
+            if not _metric:
+                _metric = 'duration'
+
+            start = timer()
+            ret = func(*args, **kwargs)
+            duration = timer() - start
+            localtime = datetime.utcnow()
+            send_metric.delay(environment, _metric, duration, _tags, localtime, server, port)
+            return ret
+
+        return wrapper
+    return _timeit
