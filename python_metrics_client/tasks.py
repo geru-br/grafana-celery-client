0349412778058ee68497e2f1c0c3f6fd33866d51
Fernando Fardo <ffardo@geru.com.br>
2017-10-26 14:44:45 -0200

    Removed commented debug snippet

--- python_metrics_client/tasks.py
+++ python_metrics_client/tasks.py
@@ -10,8 +10,6 @@ logger = get_task_logger(__name__)
 @shared_task(bind=True, queue='metrics_client')
 def send_data(self, measurement, tags, value, timestamp=None, url=None, timeout=None):
 
-    # from celery.contrib import rdb; rdb.set_trace()
-
     if not url:
         url = self.app.conf.python_metrics_client_url
 
