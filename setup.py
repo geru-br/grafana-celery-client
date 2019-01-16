23cce47cd015ab061ba08fca7d4783a54d8b2db7
Flavio Alles <flavio@geru.com.br>
2019-01-16 20:04:33 -0200

    Increased patch portion of version.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if os.environ.get('STANDALONE'):
 
 
 setup(name='python_metrics_client',
-      version='0.9.12',
+      version='0.9.13',
       description='python metrics client',
       classifiers=[
           "Programming Language :: Python",
