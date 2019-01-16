bee68aa936e17f1974160b8155b407ed43ed39be
Flavio Alles <flavio@geru.com.br>
2019-01-16 19:51:37 -0200

    Renamed includeme's single argument.
    - From `settings` to `config`.
    - As mandated in Pyramid's `Configurator.include` docs.

--- python_metrics_client/__init__.py
+++ python_metrics_client/__init__.py
@@ -6,19 +6,20 @@ from celery import current_app
 
 logger = logging.getLogger(__name__)
 
-def includeme(settings):
 
-    current_app.conf.python_metrics_client_url = settings.registry.settings.get('python_metrics_client.url')
-    current_app.conf.python_metrics_client_timeout = settings.registry.settings.get('python_metrics_client.timeout', 30)
+def includeme(config):
 
-    current_app.conf.metrics_server = settings.registry.settings.get('metrics_server', 'influxdb.tick-prod.geroo.com.br')
-    current_app.conf.metrics_client_protocol = settings.registry.settings.get('metrics_client_protocol', 'https')
-    current_app.conf.metrics_client_port = settings.registry.settings.get('metrics_client_port', 8086)
-    current_app.conf.metrics_client_timeout = settings.registry.settings.get('metrics_client_timeout', 30)
-    current_app.conf.metrics_client_type = settings.registry.settings.get('metrics_client_type', 'influxdb')
-    current_app.conf.metrics_user = settings.registry.settings.get('metrics_client_type', 'root')
-    current_app.conf.metrics_password = settings.registry.settings.get('metrics_client_type', 'root')
-    current_app.conf.metrics_environment = settings.registry.settings.get('metrics_environment', 'production')
+    current_app.conf.python_metrics_client_url = config.registry.settings.get('python_metrics_client.url')
+    current_app.conf.python_metrics_client_timeout = config.registry.settings.get('python_metrics_client.timeout', 30)
+
+    current_app.conf.metrics_server = config.registry.settings.get('metrics_server', 'influxdb.tick-prod.geroo.com.br')
+    current_app.conf.metrics_client_protocol = config.registry.settings.get('metrics_client_protocol', 'https')
+    current_app.conf.metrics_client_port = config.registry.settings.get('metrics_client_port', 8086)
+    current_app.conf.metrics_client_timeout = config.registry.settings.get('metrics_client_timeout', 30)
+    current_app.conf.metrics_client_type = config.registry.settings.get('metrics_client_type', 'influxdb')
+    current_app.conf.metrics_user = config.registry.settings.get('metrics_client_type', 'root')
+    current_app.conf.metrics_password = config.registry.settings.get('metrics_client_type', 'root')
+    current_app.conf.metrics_environment = config.registry.settings.get('metrics_environment', 'production')
 
     logger.info('metrics-client config: server: {}://{}:{}'.format(
         current_app.conf.metrics_client_protocol,
@@ -30,4 +31,5 @@ def includeme(settings):
         current_app.conf.metrics_client_type,
         current_app.conf.metrics_environment
     ))
-    settings.scan()
+
+    config.scan()
